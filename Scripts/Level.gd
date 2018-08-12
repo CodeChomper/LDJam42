43a5083cabe296ca498b1c5149b867975c4fca1c
CodeChomper <jason@codechomper.com>
2018-08-11 18:31:08 -0700

    started itch page

---
+++ Scripts/Level.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+const GRAVITY = Vector2(0,1)
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	$Ship.velocity += GRAVITY
